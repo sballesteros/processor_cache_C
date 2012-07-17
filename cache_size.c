4d92965d51435872df2c4a1961fb6e04dc1a109b
Sebastien Ballesteros <sebastien.ballesteros@gmail.com>
2012-07-16 23:44:45 -0400

    commented code

--- cache_size.c
+++ cache_size.c
@@ -13,7 +13,7 @@
 //cache sizes 
 ////////////////////////////////////////////
 //NOTE: cache line is 64 bytes (obtained by running sysctl -a | grep cache) and an int is 4 byte => 16 int to fill cache line
-
+//also, L1 cache is 32K, L2 256K and L3 3M
 
 int64_t s_clock (void)
 {
@@ -23,33 +23,30 @@ int64_t s_clock (void)
     return (int64_t) (tv.tv_sec * 1000 + tv.tv_usec / 1000);
 }
 
-
 int main(int argc, char *argv[])
 {
-
     //print size of data type
     printf("sizeof(int) = %lu\n", sizeof(int));
 
     int r, s, i;
-    int N_REP = 1; //number of repetitions
-    for (s=0; s<18; s++){
-	int mysize = (int) pow(2.0, s)*1024;
-	int *tab = malloc( (mysize/4) *sizeof(int));
+    int N_REP = 4; //number of repetitions
 
-	double tavg = 0;	
+    for (s=0; s<18; s++) {
+	int mem_size = (int) pow(2.0, s)*1024; //in bytes
+	int *tab = malloc( mem_size );
+	
+	double tavg = 0.0;	
 	for (r=0; r < N_REP; r++) { //repetitions (for time measurement)
 	    int64_t t0 = s_clock ();	    
 	    for (i=0; i< (100*1024*1024) ; i++){		
-		tab[(i * 16) % (mysize/4)]++;
+		tab[(i * 16) % (mem_size/4) ]++; //an int is 4 bytes so there are only mem_size/4 elements in the array "tab". Also we go by 16 as the cacheline is 64kb (we want to force to change cache lines if needed)
 	    }
 	    tavg += (double) (s_clock()-t0);
 	}
-	printf("size: %d kB time: %g ms\n", mysize/1024, tavg/(double) N_REP);
+	printf("size: %d kB time: %g ms\n", mem_size/1024, tavg/(double) N_REP);
 	free(tab);
     }
 
-
     return 0;    
-    
 }
 
